5566db11771ce9199f1f67f3828931e7172e362a
Mahliyo <gasandigital13@gmail.com>
2026-06-29 23:11:43 +0500

    Offline star left title

--- src/components/Main/Offline/Offline.jsx
+++ src/components/Main/Offline/Offline.jsx
@@ -12,7 +12,9 @@ function Offline() {
                 <div className="offline-left">
                   <p className="offline-text">Monride store - 1901 York Street, Cir shiloh, India 560018</p>
                 </div>
-                .
+                <div className="offline-right">
+                    
+                </div>
                  </div>
             </div>
         </div>
