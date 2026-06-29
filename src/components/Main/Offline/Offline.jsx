2dc39442bf9612d63a567990cffdfe29177b2526
Mahliyo <gasandigital13@gmail.com>
2026-06-29 23:51:41 +0500

    Offline star left text

--- src/components/Main/Offline/Offline.jsx
+++ src/components/Main/Offline/Offline.jsx
@@ -5,9 +5,8 @@ function Offline() {
     <>
     <section className="offline">
         <div className="container">
-            <div className="offline-wrappers">
+            <div className="offline-wrapper">
                  <h2 className="offline-title">Our offline stores</h2>
-                <div className="offline-wrapper">
                 <div className="offline-left">
                   <p className="offline-text">Monride store - 1901 York Street, Cir shiloh, India 560018</p>
                 </div>
@@ -16,7 +15,7 @@ function Offline() {
                 </div>
                  </div>
             </div>
-        </div>
+    
     </section>
     </>
   )
