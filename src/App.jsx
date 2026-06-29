a2bfc375e9a85e00d0d3994b1b4df9aaea4bf239
Mahliyo <gasandigital13@gmail.com>
2026-06-29 22:55:31 +0500

    Offline star import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
-
+import Offline from './components/Main/Offline/Offline';
 
 import Header from '../src/components/Header/header'
 
