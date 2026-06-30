f535cd1c8be2d4e336f6ba22b5d5a7388fe1ddd9
Mahliyo <gasandigital13@gmail.com>
2026-06-30 23:16:04 +0500

    offline-text css fnish

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
-import Offline from './components/Main/Offline/Offline';
+
 
 import Header from '../src/components/Header/header'
 import Timmeles from './components/Main/Timmeles/Timmeles';
 import Discover from './components/Main/Discover/Discover'
-
+import Offline from './components/Main/Offline/Offline';
+import Grab from './components/Grab/Grab';
 function App() {
 
   return (
@@ -17,8 +18,10 @@ function App() {
      </header>
      <main>
     <Timmeles/>
-     <Offline/>
+     
      <Discover/>
+     <Offline/>
+     <Grab/>
      </main>
     </>
   )
